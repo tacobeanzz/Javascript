ec64bbf892220648059e6de424d5c6435c90c3f3
TacoBeanzz <thisistacotaco@gmail.com>
2020-03-09 04:23:12 +0000

    calc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+function calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value;
+    var calculate;
+
+    if (op == "add") {
+        calculate = a + b;
+    } else if (op == "minus") {
+        calculate = a - b;
+    } else if (op == "divide") {
+        calculate = a / b;
+    } else if (op == "multiply") {
+        calculate = a * b;
+    }
+
+    document.querySelector("#result").innerHTML = a + " " + op + " " + b + " " + "=" + " " + calculate;
+}
